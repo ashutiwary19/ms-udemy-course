11adbd3ae87167c6083c9fa79026e545a2d00749
ashutiwary19 <ashu.tiwary19@gmail.com>
2025-05-01 00:50:31 +0530

    Added spring cloud function with rest implementation

---
+++ event-driven-demo/message/src/main/java/com/ant/message/functions/MessageFunctions.java
@@ -0,0 +1,31 @@
+package com.ant.message.functions;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ant.message.dto.AccountsMsgDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class MessageFunctions {
+
+	@Bean
+	public Function<AccountsMsgDto, AccountsMsgDto> email() {
+		return accountsMsgDto -> {
+			log.info("Sending EMAIL with details : " + accountsMsgDto.toString());
+			return accountsMsgDto;
+		};
+	}
+
+	@Bean
+	public Function<AccountsMsgDto, Long> sms() {
+		return accountsMsgDto -> {
+			log.info("Sending SMS with details : " + accountsMsgDto.toString());
+			return accountsMsgDto.getAccountNumber();
+		};
+	}
+}
---
+++ event-driven-demo/message/src/test/java/com/ant/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ant.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
